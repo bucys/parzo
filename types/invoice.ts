9c5216b45289c78fe57b1523183afc4e9b314c19
Vytautas Bucys <vytautas@Vytautass-MacBook-Air.local>
2026-04-23 21:43:53 +0300

    First commit

---
+++ types/invoice.ts
@@ -0,0 +1,9 @@
+export type InvoiceData = {
+  supplierName: string | null;
+  invoiceNumber: string | null;
+  invoiceDate: string | null;
+  totalAmount: number | null;
+  vatAmount: number | null;
+  currency: string | null;
+  notes: string | null;
+};
